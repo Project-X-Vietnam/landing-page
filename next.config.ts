d6b0776a188eaebcaa2d572b88f090d717eb244c
Song Khoi <songkhoi123@gmail.com>
2025-01-19 18:47:33 +0700

    fix: redirect

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,20 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  async redirects() {
+    return [
+      {
+        source: '/sfp2025',
+        destination: '/sfp2025/form',
+        permanent: true,
+      },
+      {
+        source: '/',
+        destination: '/sfp2025/form',
+        permanent: true,
+      },
+    ];
+  },
   async rewrites() {
     return [
       {
