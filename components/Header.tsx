aff0844d1fa25b5eb68290a52d0228a3ba9876dc
Khoi Nguyen Tinh Song <songkhoi123@gmail.com>
2025-01-19 20:25:30 +0700

    Update Header.tsx

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,6 +1,6 @@
 import { Button } from "@/components/ui/button";
 import { useState, useEffect } from "react";
-
+//
 export default function Header() {
   const [timeRemaining, setTimeRemaining] = useState("");
   const [isMobile, setIsMobile] = useState(false);
